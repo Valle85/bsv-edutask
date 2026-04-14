3a3a9e208cf6770e3bb5ce3be6ee2a35f6b143c6
Bjarnehall <Jonas.bjarnehall@outlook.com>
2026-04-14 10:17:28 +0200

    added test for print

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -21,6 +21,7 @@ def test_one_user(controller, mock_dao):
 
     assert result == {"email": "test@test.com", "name": "Test User"}
 
+
 # Test case for invalid email address
 @pytest.mark.unit
 def test_invalid_email(controller):
@@ -28,6 +29,7 @@ def test_invalid_email(controller):
         controller.get_user_by_email("test.com")
 
 # Test case for multiple users with the same email address
+    # Return first username
 @pytest.mark.unit
 def test_multiple_users(controller, mock_dao):
     mock_dao.find.return_value = [
@@ -39,6 +41,20 @@ def test_multiple_users(controller, mock_dao):
 
     assert result == {"email": "test@test.com", "name": "User1"}
 
+# Test case for multiple users with the same email address
+    # Function prints warning when multiple users found
+@pytest.mark.unit
+def test_multiple_users_print(controller, mock_dao, capsys):
+    mock_dao.find.return_value = [
+        {"email": "test@test.com", "name": "User1"},
+        {"email": "test@test.com", "name": "User2"}
+    ]
+
+    result = controller.get_user_by_email("test@test.com")
+    captured = capsys.readouterr()
+
+    assert "Error" in captured.out
+
 # Test case for no user found with the given email address
 # The test case for no user found fails due to an IndexError. 
 # The function attempts to access the first element of an empty list 
@@ -46,7 +62,6 @@ def test_multiple_users(controller, mock_dao):
 @pytest.mark.unit
 def test_no_user(controller, mock_dao):
     mock_dao.find.return_value = []
-
     result = controller.get_user_by_email("test@test.com")
 
     assert result is None
@@ -54,7 +69,7 @@ def test_no_user(controller, mock_dao):
 # Test case for database error during the search operation
 @pytest.mark.unit
 def test_db_fail(controller, mock_dao):
-    mock_dao.find.side_effect = Exception("something went wrong")
+    mock_dao.find.side_effect = Exception()
 
     with pytest.raises(Exception):
         controller.get_user_by_email("test@test.com")
