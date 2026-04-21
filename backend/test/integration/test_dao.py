12023401ba9a3554cff40c7a522501dc501360d5
Bjarnehall <Jonas.bjarnehall@outlook.com>
2026-04-21 08:55:57 +0200

    split test for one assert in each test case

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -1,28 +1,3 @@
-
-# Create Method
-# Belongs to class DAO
-
-# Method creates a new document in the collection associated to this data access object.
-# Test  should cover the following.
-
-# Test
-    # DAO.create with all required fields -> should succed
-# Test
-    # DAO.create with one field wrong -> should fail with writeError
-# Test
-    # DAO.create with correct BSON data -> should succed
-# Test
-    # DAO.create with wrong BSON data -> should fail with writeError
-# Test
-    # DAO.create with boolean property -> should succed
-# Test
-    # DAO.create with wrong boolean property -> should fail with writeError
-# Test
-    # DAO.create two documents with diffrent values for unique identifiers -> should succed
-# Test 
-    # DAO.create two documents with same values for unique fields -> should fail with writeError
-        # (This test fails probably faulth in code)
-
 import os
 import pytest
 from src.util.dao import DAO
@@ -45,11 +20,18 @@ def dao_todo():
 
 
 # Creates a video document with given url as input
-    # Expects to get a object with _id and url back
+    # Expects to get a object with _id
 @pytest.mark.integration
-def test_create_video_document_valid(dao_video):
+def test_create_video_document_valid_id(dao_video):
     result = dao_video.create({'url': 'www.youtube.com//example1'})
-    assert result['_id'] is not None and result['url'] == 'www.youtube.com//example1'
+    assert result['_id'] is not None
+
+# Creates a video document with given url as input
+    # Expects to get a object with url back
+@pytest.mark.integration
+def test_create_video_document_valid_url(dao_video):
+    result = dao_video.create({'url': 'www.youtube.com//example1'})
+    assert result['url'] == 'www.youtube.com//example1'
 
 # Creates a video document with a unvalid url as input
     # Expects to get a writeError back
@@ -60,11 +42,25 @@ def test_create_video_document_unvalid(dao_video):
 
 
 # Creates a todo document with description = "Vattna blommor" and done = True
-    # Expects to get a object with _id, correct description and done = True
+    # Expects to get a object with _id
 @pytest.mark.integration
-def test_create_todo_document_valid(dao_todo):
+def test_create_todo_document_valid_id(dao_todo):
     result = dao_todo.create({'description': 'Vattna blommor', 'done': True})
-    assert result['_id'] is not None and result['description'] == 'Vattna blommor' and result['done'] == True
+    assert result['_id'] is not None
+
+# Creates a todo document with description = "Vattna blommor" and done = True
+    # Expects to get a object with correct description
+@pytest.mark.integration
+def test_create_todo_document_valid_description(dao_todo):
+    result = dao_todo.create({'description': 'Vattna blommor', 'done': True})
+    assert result['description'] == 'Vattna blommor'
+
+# Creates a todo document with description = "Vattna blommor" and done = True
+    # Expects to get a object with done = True
+@pytest.mark.integration
+def test_create_todo_document_valid_bool(dao_todo):
+    result = dao_todo.create({'description': 'Vattna blommor', 'done': True})
+    assert result['done'] == True
 
 # Creates a todo document with description = "Vattna blommor" and done = "Ja"
     # Expects to get a writeError back
@@ -79,7 +75,7 @@ def test_create_todo_document_unvalid(dao_todo):
 def test_create_two_documents_unique(dao_todo):
     dao_todo.create({'description': 'Vattna blommor', 'done': False})
     result = dao_todo.create({'description': 'Klipp gräset', 'done': True})    
-    assert result['_id'] is not None and result['description'] == 'Klipp gräset' and result['done'] == True
+    assert result['description'] == 'Klipp gräset'
 
 # Creates two todo document with same description
     # Expect to get writeError due to unique identifier missused
