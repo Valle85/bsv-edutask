a17df0dc6ede61a434be1ac87a8c353f16f3d1b5
Bjarnehall <Jonas.bjarnehall@outlook.com>
2026-05-15 12:09:23 +0200

    fixed get_user_by_email_bug

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -32,9 +32,11 @@ class UserController(Controller):
             users = self.dao.find({'email': email})
             if len(users) == 1:
                 return users[0]
+            # NEW
+            elif len(users) > 1:
+                return users[0]
             else:
-                print(f'Error: more than one user found with mail {email}')
-                return users[0]    
+                return None
         except Exception as e:
             raise
 
